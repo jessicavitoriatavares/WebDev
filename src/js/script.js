a94f28a79c3f18819e438ad7c0a0a76e874ec920
jessica <jessica.vitoria.tavares044@gmail.com>
2025-04-01 09:10:11 -0300

    declaração de variáveis

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+// DECLARAÇÔES DE VARIÁVEIS
+
+var nome ="Fiap";
+console.log(nome)
+
+let idade = 20;
+console.log(idade)
+
+const sobrenome = "Tavares";
+console.log(sobrenome)
+
+// undefined
+let aula;
+console.log(aula)
+// null
+let valor = null;
+console.log(valor)
+// Objeto
+let exemplo1={};
+// Array
+let exemplo2=[];
