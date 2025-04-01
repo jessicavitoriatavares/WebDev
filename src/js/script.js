f9c8327fe23eacf1d3d912269530179db4cfec6f
jessica <jessica.vitoria.tavares044@gmail.com>
2025-04-01 09:50:04 -0300

    Adicionando operadores aritméticos e lógicos

--- src/js/script.js
+++ src/js/script.js
@@ -45,3 +45,41 @@ console.log(numFloat2.toString());
 
 let numInt = 100;
 console.log(numInt.toString())
+
+
+// MÉTODOS PARTE 1
+
+// length --> verifica o tamanho da string
+
+let frase = "O mundo da tecnologia";
+console.log(frase.length)
+
+// indexOf --> retorna um trecho de um texto
+
+let texto = "Programação sustentável"
+console.log(texto.indexOf("g"))
+
+// slice -->  retorna parte de um texto, apontando o inicio e o final
+
+let info = "processamento de ponta";
+console.log(info.slice(15,20))
+
+// OPERADORES ARITIMÉTICOS
+
+const num1 = 10;
+const num2 = 20;
+
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+
+//OPERADORES LÓGICOS 
+
+const num3 = 20;
+const num4 = 30;
+
+console.log(num3 > num4)
+console.log(num3 < num4 && num4 < 50)
+console.log(num3 > num4 || num4 > num3)
+console.log(num3 == num4 || num4 <= num3)
